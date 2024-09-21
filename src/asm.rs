f3229cf5a1c1840fcf495aa5e9ce4625638d2276
boribs <christumer@gmail.com>
2024-09-21 09:28:19 -0600

    load Mul instruction from mem

--- src/asm.rs
+++ src/asm.rs
@@ -213,7 +213,7 @@ impl cpu::Instruction {
                 let b = get_cr(&mut bytes, mem, b_reg, index)?;
                 cpu::Instruction::Ld(a, b)
             }
-            2..=3 => {
+            2..=4 => {
                 let a = get_cr(&mut bytes, mem, a_reg, index + 1)?;
                 let b = cpu::Reg::from_code(mem.read(index + bytes))?;
                 bytes += 1;
@@ -221,6 +221,7 @@ impl cpu::Instruction {
                 match instr {
                     2 => cpu::Instruction::Sum(a, b),
                     3 => cpu::Instruction::Sub(a, b),
+                    4 => cpu::Instruction::Mul(a, b),
                     _ => unimplemented!(),
                 }
             }
@@ -638,6 +639,35 @@ mod read_from_mem {
         let expected = [
             (Instruction::Sub(CR::Register(Reg::A), Reg::B), 3),
             (Instruction::Sub(CR::Constant(0xab), Reg::AL), 4),
+        ];
+
+        let actual = [
+            Instruction::from_mem(&mem, 0),
+            Instruction::from_mem(&mem, 3),
+        ];
+
+        for i in 0..expected.len() {
+            assert!(actual[i].is_ok());
+            let a = actual[i].unwrap();
+            assert_eq!(a, expected[i]);
+        }
+    }
+
+    #[test]
+    fn read_mul() {
+        let mem = Mem::set(vec![
+            0b00100011,
+            Reg::A.code(),
+            Reg::B.code(),
+            0b00100001,
+            0,
+            0xab,
+            Reg::AL.code(),
+        ]);
+
+        let expected = [
+            (Instruction::Mul(CR::Register(Reg::A), Reg::B), 3),
+            (Instruction::Mul(CR::Constant(0xab), Reg::AL), 4),
         ];
 
         let actual = [
